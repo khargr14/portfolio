b9c1ca427f2d39f0a957cb3921de33004c194e2d
khargr14 <kay_hargrove@yahoo.com>
2022-03-30 03:19:53 -0400

    layout page for products

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
+import Intro from "./components/intro/Intro";
+import ProductList from "./components/productList/ProductList";
+
 
 const App = () => {
   return <div>
     <Intro />
     <About />
+    <ProductList />
     </div>;
 }
 
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "C:\Users\kay_h\OneDrive\Documents\kids stuff",
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "",
+      link: "http://lama.dev",
+    },
+  ];
